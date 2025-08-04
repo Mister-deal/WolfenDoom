b38e27d5aaa92e7fd3e2e4f435e2d004aecf0a6b
Mister-deal <max-94400@hotmail.fr>
2025-08-04 18:34:55 +0200

    ajout projet WolfenDoom

---
+++ wolfendoom/Player.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+	public const float TurnSpeed = 0.05f;
+	
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		if(Input.IsActionPressed("ui_left")){
+			RotateY(TurnSpeed);
+		};
+		
+		if(Input.IsActionPressed("ui_right")){
+			RotateY(-TurnSpeed);
+		}
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
